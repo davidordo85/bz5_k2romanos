194b0e34290cf417e1960c7f94376c6f62351ac5
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-05-01 12:45:44 +0200

    Versión preliminar

---
+++ main.py
@@ -0,0 +1,5 @@
+import romanos
+
+numero_nRomano = input('Dame un dígito Romano: ')
+
+print(romanos.nRomano_a_entero(numero_nRomano))
---
+++ romanos.py
@@ -0,0 +1,34 @@
+romanos = {'M':1000,
+           'D':500,
+           'C':100,
+           'L':50,
+           'X':10,
+           'V':5,
+           'I':1,
+           '': None
+}
+
+
+def nRomano_a_entero(numero_nRomano):    
+    if numero_nRomano == '':
+        return 'Error en formato'   
+
+    entero = 0
+    numRepes = 1
+    letraAnt = ''
+    for letra in numero_nRomano:
+        if letra == letraAnt and numRepes == 3:
+            return 'Error en formato'
+        elif letra == letraAnt :
+            numRepes += 1
+        else:
+            numRepes = 1
+            
+        if letra in romanos:
+            entero += romanos[letra]            
+        else:
+            return 'Error en formato'
+
+        letraAnt = letra
+    
+    return entero
