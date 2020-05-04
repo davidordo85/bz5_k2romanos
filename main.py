f041c79f15204765bffedb80f69a85a024e8cc44
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-05-05 00:38:57 +0200

    romanos completo

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import romanos
 
 numero_nRomano = input('Dame un dígito Romano: ')
 
-print(romanos.nRomano_a_entero(numero_nRomano))
+print('*', romanos.nRomano_a_entero(numero_nRomano), '*')
--- romanos.py
+++ romanos.py
@@ -4,10 +4,10 @@ romanos = {'M':1000,
            'L':50,
            'X':10,
            'V':5,
-           'I':1,
-           '': None
+           'I':1,           
 }
 
+existen = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
 
 def nRomano_a_entero(numero_nRomano):    
     if numero_nRomano == '':
@@ -16,18 +16,29 @@ def nRomano_a_entero(numero_nRomano):
     entero = 0
     numRepes = 1
     letraAnt = ''
+    fueResta = False
     for letra in numero_nRomano:
+
+            
+        if letra in romanos:
+            if letraAnt == '' or romanos[letraAnt] >= romanos[letra]:
+                entero += romanos[letra]
+                fueResta = False
+            else:
+                if letraAnt + letra in existen and numRepes < 2 and not fueResta:
+                    entero = entero - romanos[letraAnt] * 2 + romanos[letra]
+                    fueResta = True
+                else:
+                    return 'Error en formato'       
+        else:
+            return 'Error en formato'
+
         if letra == letraAnt and numRepes == 3:
             return 'Error en formato'
         elif letra == letraAnt :
             numRepes += 1
         else:
             numRepes = 1
-            
-        if letra in romanos:
-            entero += romanos[letra]            
-        else:
-            return 'Error en formato'
 
         letraAnt = letra
     
