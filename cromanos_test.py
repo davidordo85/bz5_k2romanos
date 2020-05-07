bfd538277dfe04f87428b42adb69e8aa6e58267c
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-05-08 00:29:51 +0200

    Fin clase Romanos

--- cromanos_test.py
+++ cromanos_test.py
@@ -9,8 +9,7 @@ class RomanNumberTest(unittest.TestCase):
 
         snr = cromanos.RomanNumber('XXIV')
         self.assertEqual(snr.value, 24)
-        self.assertEqual(snr.rvalue, 'XXIV')
-        
+        self.assertEqual(snr.rvalue, 'XXIV')        
 
         tnr = cromanos.RomanNumber('XXXX')
         self.assertEqual(tnr.value, 'Error en formato')
@@ -24,7 +23,33 @@ class RomanNumberTest(unittest.TestCase):
         self.assertEqual(qnr.value, 'Overflow')
         self.assertEqual(qnr.rvalue, 'Overflow')
 
+    def test_representation(self):
+        nr = cromanos.RomanNumber(25)
+        self.assertEqual(str(nr), 'XXV')
+        #self.assertEqual(nr, 'XXV')
 
+    def test_equal_roman(self):
+        nr1 = cromanos.RomanNumber(25)
+        nr2 = cromanos.RomanNumber('XXV')
+        self.assertEqual(nr1, nr2)
+
+    def test_add(self):
+        nr1 = cromanos.RomanNumber(1)
+        nr2 = cromanos.RomanNumber('XXIV')
+        nr3 = nr1 + nr2
+        self.assertEqual(nr1+nr2, cromanos.RomanNumber(25))
+        self.assertEqual(nr3.value, 25)
+        self.assertTrue(isinstance(nr3, cromanos.RomanNumber))
+
+    def test_add_integer(self):
+        nr1 = cromanos.RomanNumber('XXIII')
+        nr3 = nr1 + 1
+        self.assertEqual(nr3.value, 24)
+        self.assertTrue(isinstance(nr3, cromanos.RomanNumber))
+
+        nr2 = 1 + nr1
+        self.assertEqual(nr2.value, 24)
+        self.assertTrue(isinstance(nr2, cromanos.RomanNumber))
 
 
 if __name__ == '__main__':
